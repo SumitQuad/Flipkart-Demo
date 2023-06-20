9ad3fcdb91e893fd64de2ecc4fe52ebef2b18c70
Sumit <sumit.5467d@gmail.com>
2023-06-20 10:01:37 +0530

    changes-in-rangeslider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
+import "./App.css";
 import CustomerRatingFilter from "./Components/Filters/CustomerRating/customerrating";
 import WashingMachineList from "./Components/WashingCardDetails/washingcarddetails";
-import RangeSlider from './Components/Filters/RangeSlider/RangeSlider';
+import RangeSlider from "./Components/Filters/RangeSlider/RangeSlider";
 import SortBySection from "./Components/Sorting/SortBySection/SortBySection";
 import data from "./Data/data.json";
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   const [minValue, setMinValue] = useState(0);
   const [maxValue, setMaxValue] = useState(30000);
   const [filteredWashingMachines, setFilteredWashingMachines] = useState(data);
-  const [sortedWashingMachines, setSortedWashingMachines] = useState([]);
-  const [filterpopularity, setFilterPopularity] = useState([]);
-  const [activeSort, setActiveSort] = useState("popularity");
   const [selectedRatings, setSelectedRatings] = useState([]);
   const [showDiscounted, setShowDiscounted] = useState(false);
-
 
   const handleRatingFilterChange = (rating) => {
     if (selectedRatings.includes(rating)) {
@@ -61,49 +57,17 @@ function App() {
       }
 
       if (selectedRatings.length > 0) {
-        const selectedRatingValue = Math.min(...selectedRatings.map((rating) => parseInt(rating)));
+        const selectedRatingValue = Math.min(
+          ...selectedRatings.map((rating) => parseInt(rating))
+        );
 
-        updatedList = updatedList.filter((data) =>
-          parseInt(data.rating.charAt(0)) >= selectedRatingValue
+        updatedList = updatedList.filter(
+          (data) => parseInt(data.rating.charAt(0)) >= selectedRatingValue
         );
       }
     }
 
     setFilteredWashingMachines(updatedList);
-    setSortedWashingMachines(updatedList);
-    setFilterPopularity(updatedList);
-  };
-
-  const sortLowToHigh = () => {
-    const sortedList = [...filteredWashingMachines].sort((a, b) => a.price - b.price);
-    setSortedWashingMachines(sortedList);
-    setActiveSort("lowToHigh");
-  };
-
-  const sortHighToLow = () => {
-    const sortedList = [...filteredWashingMachines].sort((a, b) => b.price - a.price);
-    setSortedWashingMachines(sortedList);
-    setActiveSort("highToLow");
-  };
-
-  const handleSortOnPopularity = () => {
-    const sortedList = [...filteredWashingMachines].sort((a, b) => {
-      const ratingA = parseFloat(a.rating);
-      const ratingB = parseFloat(b.rating);
-      return ratingB - ratingA;
-    });
-    setSortedWashingMachines(sortedList);
-    setActiveSort("popularity");
-  };
-
-  const sortDiscounted = () => {
-    const sortedList = [...filteredWashingMachines].sort((a, b) => {
-      const discountPercentageA = calculateDiscountPercentage(a.price, a.originalprice);
-      const discountPercentageB = calculateDiscountPercentage(b.price, b.originalprice);
-      return discountPercentageB - discountPercentageA;
-    });
-    setSortedWashingMachines(sortedList);
-    setActiveSort("discount");
   };
 
   const handleChange = (values) => {
@@ -119,7 +83,7 @@ function App() {
     <div className="App">
       <Container fluid className="mt-2">
         <Row>
-          <Col lg={3}>
+          <Col lg={3} className="sticky-column">
             <Card className="card-input">
               <h3>Filters</h3>
               <hr />
@@ -144,20 +108,23 @@ function App() {
           </Col>
           <Col lg={9}>
             <Card className="p-2">
-              <h3 className="card-heading">Washing Machines <span>(Showing 1 – 13 products of {data.length} products)</span></h3>
+              <h3 className="card-heading">
+                Washing Machines{" "}
+                <span>
+                  (Showing 1 – 13 products of {data.length} products)
+                </span>
+              </h3>
               <div className="sort-container">
                 <SortBySection
-                  activeSort={activeSort}
-                  handleSortOnPopularity={handleSortOnPopularity}
-                  sortLowToHigh={sortLowToHigh}
-                  sortHighToLow={sortHighToLow}
-                  sortDiscounted={sortDiscounted}
+                  filteredWashingMachines={filteredWashingMachines}
+                  setFilteredWashingMachines={setFilteredWashingMachines}
                 />
-
               </div>
 
               <Container>
-                <WashingMachineList washingMachines={sortedWashingMachines} />
+                <WashingMachineList
+                  washingMachines={filteredWashingMachines}
+                />
               </Container>
             </Card>
           </Col>
